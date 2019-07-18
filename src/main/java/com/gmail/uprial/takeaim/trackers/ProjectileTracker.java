dda8fafa3fdda59740293ac293788abcb2f3cc74
Ilya Roslyakov <uprial@gmail.com>
2019-07-18 23:15:22 +0300

    Track projectiles better

--- src/main/java/com/gmail/uprial/takeaim/trackers/ProjectileTracker.java
+++ src/main/java/com/gmail/uprial/takeaim/trackers/ProjectileTracker.java
@@ -3,20 +3,36 @@ package com.gmail.uprial.takeaim.trackers;
 import com.gmail.uprial.takeaim.TakeAim;
 import com.gmail.uprial.takeaim.common.CustomLogger;
 import org.bukkit.entity.Projectile;
+import org.bukkit.util.Vector;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static com.gmail.uprial.takeaim.common.Formatter.format;
 
 public class ProjectileTracker extends AbstractTracker {
+    private class ProjectileInfo {
+        private final long id;
+        private final Vector startPosition;
+
+        private final AtomicLong ticksInFly = new AtomicLong();
+
+        ProjectileInfo(long id, Vector startPosition) {
+            this.id = id;
+            this.startPosition = startPosition;
+        }
+    }
+
     private static final int INTERVAL = 1;
 
     private final TakeAim plugin;
     private final CustomLogger customLogger;
 
-    private final Map<Projectile,Integer> projectiles = new HashMap<>();
-    private final AtomicInteger projectileIdIncrement = new AtomicInteger();
+    private final Map<Projectile,ProjectileInfo> projectiles = new HashMap<>();
+    private final AtomicLong projectileIdIncrement = new AtomicLong();
 
     public ProjectileTracker(TakeAim plugin, CustomLogger customLogger) {
         super(plugin, INTERVAL);
@@ -29,21 +45,31 @@ public class ProjectileTracker extends AbstractTracker {
 
     public void onLaunch(Projectile projectile) {
         if(enabled) {
-            int projectileId = projectileIdIncrement.incrementAndGet();
-            log(projectileId, projectile, "has been launched");
+            long projectileId = projectileIdIncrement.incrementAndGet();
+            ProjectileInfo info = new ProjectileInfo(
+                    projectileId,
+                    projectile.getLocation().toVector());
+            log(info, projectile, "has been launched");
 
-            projectiles.put(projectile, projectileId);
+            projectiles.put(projectile, info);
         }
     }
 
     public void onHit(Projectile projectile) {
         if(enabled) {
-            Integer projectileId = projectiles.get(projectile);
-            if(projectileId == null) {
-                log(0, projectile, "hit the target");
+            ProjectileInfo info = projectiles.get(projectile);
+            if(info == null) {
+                log(null, projectile, "hit the target");
 
             } else {
-                log(projectileId, projectile, "hit the target");
+                log(info, projectile, "hit the target");
+
+                Vector avgVelocity = projectile.getLocation().toVector()
+                        .subtract(info.startPosition)
+                        .multiply(1.0D / Math.max(1.0D, info.ticksInFly.get()));
+
+                customLogger.debug(String.format("#%d %s average velocity: %s",
+                        info.id, format(projectile), format(avgVelocity)));
 
                 projectiles.remove(projectile);
             }
@@ -54,13 +80,18 @@ public class ProjectileTracker extends AbstractTracker {
     public void run() {
         if(enabled) {
             List<Projectile> onHit = new ArrayList<>();
-            for (Map.Entry<Projectile, Integer> entry : projectiles.entrySet()) {
+            for (Map.Entry<Projectile, ProjectileInfo> entry : projectiles.entrySet()) {
                 Projectile projectile = entry.getKey();
 
                 if (projectile.isDead() || !projectile.isValid() || projectile.isOnGround()) {
                     onHit.add(projectile);
                 } else {
-                    log(entry.getValue(), projectile, "is flying");
+                    ProjectileInfo info = entry.getValue();
+                    if((info.ticksInFly.get() > 0)
+                        || (!projectile.getLocation().toVector().equals(info.startPosition))) {
+                        info.ticksInFly.incrementAndGet();
+                    }
+                    log(info, projectile, "is flying");
 
                 }
             }
@@ -73,7 +104,7 @@ public class ProjectileTracker extends AbstractTracker {
     @Override
     protected void clear() {
         projectiles.clear();
-        projectileIdIncrement.set(0);
+        projectileIdIncrement.set(0L);
     }
 
     @Override
@@ -81,8 +112,13 @@ public class ProjectileTracker extends AbstractTracker {
         return plugin.getTakeAimConfig().isEnabled() && customLogger.isDebugMode();
     }
 
-    private void log(int projectileId, Projectile projectile, String action) {
-        customLogger.debug(String.format("#%d %s %s with %s",
-                projectileId, format(projectile), action, format(projectile.getVelocity())));
+    private void log(ProjectileInfo info, Projectile projectile, String action) {
+        if(info == null) {
+            customLogger.debug(String.format("#? %s %s with %s, tick #?",
+                    format(projectile), action, format(projectile.getVelocity())));
+        } else {
+            customLogger.debug(String.format("#%d %s %s with %s, tick #%d",
+                    info.id, format(projectile), action, format(projectile.getVelocity()), info.ticksInFly.get()));
+        }
     }
 }
